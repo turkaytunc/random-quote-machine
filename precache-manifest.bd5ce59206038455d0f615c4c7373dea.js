a05982a3382e0f0c728f3d54457a8e22d300337b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-01 05:58:47 +0000

    Deploy turkaytunc/random-quote-machine to github.com/turkaytunc/random-quote-machine.git:gh-pages

---
+++ precache-manifest.bd5ce59206038455d0f615c4c7373dea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d62809ccd1e3187d5fc06ff535c64d0",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "61f1f5c229a4974d8404",
+    "url": "/random-quote-machine/static/css/main.a24ac0b7.chunk.css"
+  },
+  {
+    "revision": "9bdf7f0707530342ad86",
+    "url": "/random-quote-machine/static/js/2.ddb79d94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.ddb79d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f1f5c229a4974d8404",
+    "url": "/random-quote-machine/static/js/main.ca089592.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
